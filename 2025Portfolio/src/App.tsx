b1b2c4f18f3423eb9fe86091b27034d5c98f56b7
JeongHui-seong <jgmltjd99@naver.com>
2025-09-17 11:29:35 +0900

    날씨 api 호출 및 ui 표현

---
+++ 2025Portfolio/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import WeatherBox from './components/WeatherBox'
+
+function App() {
+  return (
+    <div>
+      <div className="left-float">
+        <WeatherBox />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ 2025Portfolio/src/main.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import 'react-loading-skeleton/dist/skeleton.css';
+
+createRoot(document.getElementById('root')!).render(
+    <App />
+)
